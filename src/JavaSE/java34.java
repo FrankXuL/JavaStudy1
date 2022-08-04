28f5c13fd7e5ab00674bc2b8930c012b91535a7e
@CX330_x <2810288656@qq.com>
2022-08-04 23:29:13 +0800

    日常

---
+++ src/JavaSE/java34.java
@@ -0,0 +1,42 @@
+package JavaSE;
+
+import java.util.Scanner;
+
+/**
+ * @title: java35
+ * @Author Xu
+ * @Date: 2022/8/4 23:24
+ * @Version 1.0
+ */
+public class java34 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            Data data = new Data(x, y);
+            System.out.println(data.getX() + data.getY());
+        }
+    }
+
+}
+
+class Data {
+
+    private int x;
+    private int y;
+
+    public Data(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
