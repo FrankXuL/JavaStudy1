0eab769c33cbddfc914d87663802737747041ba2
@CX330_x <2810288656@qq.com>
2022-09-27 21:08:39 +0800

    日常

--- src/JavaDS/MyBinaryTree.java
+++ src/JavaDS/MyBinaryTree.java
@@ -1,5 +1,10 @@
 package JavaDS;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @title: MyBinaryTree
  * @Author Xu
@@ -49,41 +54,150 @@ public class MyBinaryTree {
         System.out.print(root.val + " ");
     }
 
-    // 获取树中节点的个数
+    // 获取树中节点的  个数
     public int size(TreeNode root) {
-        return -1;
+        if (root == null) {
+            return 0;
+        }
+        return size(root.left) + size(root.right) + 1;
     }
 
-    // 获取叶子节点的个数
+    public static int nodeSize = 0;
+
+    public int size2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        nodeSize++;
+        size(root.left);
+        size(root.right);
+        return nodeSize;
+    }
+
+    /*
+    获取叶子节点的个数：子问题
+     */
     public int getLeafNodeCount(TreeNode root) {
-        return -1;
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return getLeafNodeCount(root.left)
+                + getLeafNodeCount(root.right) + 1;
     }
 
+    /*
+     获取叶子节点的个数：遍历思路
+     */
+    public static int leafSize = 0;
+
+    public void getLeafNodeCount2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            leafSize++;
+        }
+        getLeafNodeCount2(root.left);
+        getLeafNodeCount2(root.right);
+    }
     // 子问题思路-求叶子结点个数
 
     // 获取第K层节点的个数
-    public int getKLevelNodeCount(TreeNode root) {
-        return -1;
+    public int getKLevelNodeCount(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        return getKLevelNodeCount(root.left, k - 1)
+                + getKLevelNodeCount(root.right, k - 1) + 1;
     }
 
     // 获取二叉树的高度
     public int getHeight(TreeNode root) {
-        return -1;
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
     }
 
     // 检测值为value的元素是否存在
     public TreeNode find(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        TreeNode ret1 = find(root.left, val);
+        if (ret1 != null) {
+            return ret1;
+        }
+        TreeNode ret2 = find(root.right, val);
+        if (ret2 != null) {
+            return ret2;
+        }
         return null;
     }
 
     //层序遍历
     public void levelOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            System.out.print(cur + " ");
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
+    }
 
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        //如何确定每一层？
+        return list;
     }
 
     // 判断一棵树是不是完全二叉树
     public boolean isCompleteTree(TreeNode root) {
-        return false;
+        if (root == null) {
+            return false;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if(cur == null){
+                break;
+            }
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
+        if(!queue.isEmpty()){
+            TreeNode cur = queue.poll();
+            if(cur != null){
+                return  false;
+            }
+        }
+        return  true;
     }
 
 }
