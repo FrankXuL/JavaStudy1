6b1e72b42e4238b9fda9f77ff9f86cf0606c7e1a
@CX330_x <2810288656@qq.com>
2022-10-16 18:12:58 +0800

    完成大根堆的模拟实现

--- src/main/java/JavaDS/Heap.java
+++ src/main/java/JavaDS/Heap.java
@@ -1,5 +1,7 @@
 package JavaDS;
 
+import java.util.Arrays;
+
 /**
  * @title: Heap
  * @Author Xu
@@ -7,4 +9,124 @@ package JavaDS;
  * @Version 1.0
  */
 public class Heap {
+    public int[] elem;
+
+    public int usedSize;//有效的数据个数
+
+    public static final int DEFAULT_SIZE = 10;
+
+    public Heap() {
+        elem = new int[DEFAULT_SIZE];
+    }
+
+    public void initElem(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            elem[i] = array[i];
+            usedSize++;
+        }
+    }
+
+    /**
+     * 时间复杂度：O(n)
+     */
+    public void createHeap() {
+        for (int parent = (usedSize - 1 - 1) / 2; parent >= 0; parent--) {
+            //统一的调整方案
+            shiftDown(parent, usedSize);
+        }
+    }
+
+    private void shiftDown(int parent, int usedSize) {
+        int child = 2 * parent + 1; //找到左孩子
+        while (child < usedSize) {
+            ////child+1保证有右孩子,如果右孩子比左孩子大的话需要调整的是右孩子
+            if (child + 1 < usedSize && elem[child] < elem[child + 1]) {
+                child++;
+            }
+            if (elem[child] > elem[parent]) {
+                int tmp = elem[child];
+                elem[child] = elem[parent];
+                elem[parent] = tmp;
+                //继续找该结点的孩子结点
+                parent = child;
+                child = 2 * parent + 1;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public void offer(int val) {
+        if (isFull()) {
+            //扩容
+            elem = Arrays.copyOf(this.elem, 2 * this.elem.length);
+        }
+        this.elem[usedSize] = val;
+        usedSize++;
+        //想办法让他再次变成大根堆
+        shiftUp(usedSize - 1);
+    }
+
+    private void shiftUp(int child) {
+        int parent = (child - 1) / 2;
+        while (child > 0) {//parent >= 0
+            if (elem[child] > elem[parent]) {
+                int tmp = elem[child];
+                elem[child] = elem[parent];
+                elem[parent] = tmp;
+                child = parent;
+                parent = (child - 1) / 2;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return elem[0];
+    }
+
+    public boolean isFull() {
+        return usedSize == elem.length;
+    }
+
+    public boolean isEmpty() {
+        return usedSize == 0;
+    }
+
+    public void heapSort() {
+        //1.建立大根堆 O(n)
+        createHeap();
+        //2.然后排序
+        int end = usedSize - 1;
+        while (end > 0) {
+            int tmp = elem[0];
+            elem[0] = elem[end];
+            elem[end] = tmp;
+            shiftDown(0, end);
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        int[] array = {1, 8, 5, 6, 9, 13, 15, 78, 65, 98};
+        heap.initElem(array);
+        for (int i = 0; i < heap.elem.length; i++) {
+            System.out.print(heap.elem[i] + " ");
+        }
+        System.out.println();
+        heap.createHeap();
+        for (int i = 0; i < heap.elem.length; i++) {
+            System.out.print(heap.elem[i] + " ");
+        }
+        System.out.println();
+        heap.heapSort();
+        for (int i = 0; i < heap.elem.length; i++) {
+            System.out.print(heap.elem[i] + " ");
+        }
+    }
 }
--- src/main/java/JavaDS/sort.java
+++ src/main/java/JavaDS/sort.java
@@ -9,9 +9,11 @@ package JavaDS;
  */
 public class sort {
     public static void main(String[] args) {
-        //直接插入排序
         int[] arr = {13, 55, 14, 87, 1, 2, 8};
-        InsertSort(arr);
+        //InsertSort(arr);
+        //ShellSort(arr);
+        //SelectSort(arr);
+        //SelectSort2(arr);
         System.out.print("排序后:");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -24,7 +26,8 @@ public class sort {
      * @description: 直接插入排序
      * 时间复杂度: O(n^2)
      * 空间复杂度: O(1)
-     * 稳定性:不稳定
+     * 稳定性:稳定
+     * 当数据量不多，且基本上趋于有序的时候，直接插入排序是非常快
      * @author Xu
      * @date: 13/10/2022 下午 7:07
      */
@@ -46,8 +49,7 @@ public class sort {
 
     /**
      * @description: 希尔排序
-     * 时间复杂度: O(n^2)
-     * 空间复杂度: O(1)
+     * 时间复杂度: O(n^1.3)
      * 稳定性:不稳定
      * @author Xu
      * @date: 13/10/2022 下午 7:07
@@ -56,7 +58,7 @@ public class sort {
         int gap = array.length;
         while (gap > 1) {
             gap /= 2;
-            for (int i = 1; i <= gap; i++) {
+            for (int i = gap; i < array.length; i++) {
                 int tmp = array[i];
                 int j = i - gap;
                 while (j >= 0) {
@@ -65,7 +67,7 @@ public class sort {
                     } else {
                         break;
                     }
-                    j--;
+                    j -= gap;
                 }
                 array[j + gap] = tmp;
             }
@@ -89,9 +91,7 @@ public class sort {
                 }
             }
             if (minDex != i) {
-                int tmp = array[i];
-                array[i] = array[minDex];
-                array[minDex] = tmp;
+                swap(array, minDex, i);
             }
         }
     }
@@ -120,9 +120,11 @@ public class sort {
             }
             //更新最小值
             if (minIndex != left) {
-                int tmp = array[left];
-                array[left] = array[minIndex];
-                array[minIndex] = tmp;
+                //int tmp = array[left];
+                //array[left] = array[minIndex];
+                //array[minIndex] = tmp;
+                swap(array,minIndex,left);
+
             }
 
             //特殊情况 max正好是left就是把最大值更新到mindex了;
@@ -131,9 +133,10 @@ public class sort {
             }
             //更新最大值
             if (maxIndex != right) {
-                int tmp = array[right];
-                array[right] = array[maxIndex];
-                array[maxIndex] = tmp;
+                //int tmp = array[right];
+                //array[right] = array[maxIndex];
+                //array[maxIndex] = tmp;
+                swap(array,maxIndex,right);
             }
             left++;
             right--;
@@ -149,6 +152,18 @@ public class sort {
      * @date: 13/10/2022 下午 7:51
      */
     public static void BubbleSort(int[] array) {
+        for (int i = 0 ; i< array.length-1 ; i++){
+            boolean flag = false;
+            for (int j = 0; j < array.length-1-i; j++) {
+                if(array[j] > array[j+1]) {
+                    swap(array,j,j+1);
+                    flag = true;
+                }
+            }
+            if(flag == false) {
+                break;
+            }
+        }
 
     }
 
@@ -161,11 +176,16 @@ public class sort {
      * @date: 13/10/2022 下午 7:51
      */
     public static void QuickSort(int[] array) {
-        quick(array,0,array.length-1);
+        quick(array, 0, array.length - 1);
     }
 
-    public static void quick(int[] array,int start , int end){
+    public static void quick(int[] array, int start, int end) {
 
     }
 
+    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
 }
