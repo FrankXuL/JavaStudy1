2ba79cdefbe1860f80934f15b2f2003e9afdcc41
@CX330_x <2810288656@qq.com>
2022-10-17 15:35:17 +0800

    排序完结

--- src/main/java/JavaDS/sort.java
+++ src/main/java/JavaDS/sort.java
@@ -1,12 +1,15 @@
 package JavaDS;
 
 
+import java.util.Stack;
+
 /**
  * @title: sort
  * @Author Xu
  * @Date: 12/10/2022 下午 9:01
  * @Version 1.0
  */
+@SuppressWarnings({"all"})
 public class sort {
     public static void main(String[] args) {
         int[] arr = {13, 55, 14, 87, 1, 2, 8};
@@ -15,7 +18,8 @@ public class sort {
         //SelectSort(arr);
         //SelectSort2(arr);
         //HeapSort(arr);
-        QuickSort(arr);
+        //QuickSort(arr);
+        QuickSortNonR(arr);
         System.out.print("排序后:");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -244,7 +248,9 @@ public class sort {
         int index = findMidValOfIndex(array, start, end);
         swap(array, start, index);
 
-        int pivot = partitionHoare(array, start, end);  //Hoare法
+        //int pivot = partitionHoare(array, start, end);  //Hoare法
+        //int pivot = partition1(array, start, end);  //挖坑法
+        int pivot = partition2(array, start, end);  //前后指针
 
         quick(array, start, pivot - 1);
         quick(array, pivot + 1, end);
@@ -254,16 +260,45 @@ public class sort {
         int i = left;
         int pivot = array[left];
         while (left < right) {
-            while (left < right && array[right] >= pivot){
+            while (left < right && array[right] >= pivot) {
                 right--;
             }
-            while (left < right && array[left] <= pivot){
+            while (left < right && array[left] <= pivot) {
                 left++;
             }
-            swap(array,left,right);
+            swap(array, left, right);
         }
-        swap(array,left,i);
+        swap(array, left, i);
         return left;
+    }
+
+    private static int partition1(int[] array, int left, int right) {
+        int pivot = array[left];
+        while (left < right) {
+            while (left < right && array[right] >= pivot) {
+                right--;
+            }
+            array[left] = array[right];
+            while (left < right && array[left] <= pivot) {
+                left++;
+            }
+            array[right] = array[left];
+        }
+        array[left] = pivot;
+        return left;
+    }
+
+    private static int partition2(int[] array, int left, int right) {
+        int prev = left;
+        int cur = left + 1;
+        while (cur <= right) {
+            if (array[cur] < array[left] && array[++prev] != array[left]) {
+                swap(array, prev, cur);
+            }
+            cur++;
+        }
+        swap(array, prev, left);
+        return prev;
     }
 
     private static int findMidValOfIndex(int[] array, int start, int end) {
@@ -287,9 +322,142 @@ public class sort {
         }
     }
 
+    public static void QuickSortNonR(int[] array) {
+        Stack<Integer> stack = new Stack<>();
+        int start = 0;
+        int end = array.length - 1;
+        int pivot = partition1(array, start, end);
+        if (pivot > start + 1) {
+            stack.push(start);
+            stack.push(pivot - 1);
+        }
+        //2.判断右边是不是有2个元素
+        if (pivot < end - 1) {
+            stack.push(pivot + 1);
+            stack.push(end);
+        }
+        while (!stack.isEmpty()) {
+            end = stack.pop();
+            start = stack.pop();
+            pivot = partition1(array, start, end);
+            //3.判断左边是不是有2个元素
+            if (pivot > start + 1) {
+                stack.push(start);
+                stack.push(pivot - 1);
+            }
+            //4.判断右边是不是有2个元素
+            if (pivot < end - 1) {
+                stack.push(pivot + 1);
+                stack.push(end);
+            }
+        }
+    }
+
     private static void swap(int[] array, int i, int j) {
         int tmp = array[i];
         array[i] = array[j];
         array[j] = tmp;
+    }
+
+    /**
+     * @description: 归并排序
+     * 时间复杂度: O(N*logN)
+     * 空间复杂度: O(N)
+     * 稳定性:稳定的排序
+     * @author Xu
+     * @date: 17/10/2022 下午 3:05
+     */
+    public static void MergeSort(int[] array) {
+        mergeSortChild(array, 0, array.length - 1);
+    }
+
+    private static void mergeSortChild(int[] array, int left, int right) {
+        if (left == right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        mergeSortChild(array, left, mid);
+        mergeSortChild(array, mid + 1, right);
+        merge(array, left, mid, right);
+    }
+
+    private static void merge(int[] array, int left, int mid, int right) {
+        int s1 = left;
+        int e1 = mid;
+        int s2 = mid + 1;
+        int e2 = right;
+        int[] tmpArr = new int[right - left + 1];
+        int k = 0;//表示tmpArr 的下标
+        while (s1 <= e1 && s2 <= e2) {
+            if (array[s1] <= array[s2]) {
+                tmpArr[k++] = array[s1++];
+            } else {
+                tmpArr[k++] = array[s2++];
+            }
+        }
+        while (s1 <= e1) {
+            tmpArr[k++] = array[s1++];
+        }
+        while (s2 <= e2) {
+            tmpArr[k++] = array[s2++];
+        }
+        //tmpArr当中 的数据 是right  left 之间有序的数据
+        for (int i = 0; i < k; i++) {
+            array[i + left] = tmpArr[i];
+        }
+    }
+
+    public static void mergeSort(int[] array) {
+        int gap = 1;
+        while (gap < array.length){
+            for (int i = 0; i < array.length; i += gap*2) {
+                int left = i;
+                int mid = left + gap -1;
+                int right = mid+gap;
+                if(mid >= array.length) {
+                    mid = array.length-1;
+                }
+                if(right >= array.length) {
+                    right = array.length-1;
+                }
+                merge(array,left,mid,right);
+            }
+            gap *= 2;
+        }
+    }
+    
+    /** 
+     * @description: 计数排序
+     * 时间复杂度：O(n+范围) -》 范围越小，时间复杂度越小
+     * 空间复杂度：O(范围)
+     * @author Xu
+     * @date: 17/10/2022 下午 3:34
+     */ 
+    public static void countSort(int[] array) {
+        int maxVal = array[0];
+        int minVal = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > maxVal) {
+                maxVal = array[i];
+            }
+            if(array[i] < minVal) {
+                minVal = array[i];
+            }
+        }
+        int len = maxVal - minVal + 1 ;
+        int[] countArr = new int[len];
+        for (int i = 0; i < array.length; i++) {
+            int val = array[i];
+            countArr[val-minVal] ++;
+        }
+        int index = 0;
+        for (int i = 0; i < countArr.length; i++) {
+            while (countArr[i] > 0) {
+                //不敢打印
+                array[index] = i+minVal;
+                index++;
+                countArr[i]--;
+            }
+        }
     }
 }
