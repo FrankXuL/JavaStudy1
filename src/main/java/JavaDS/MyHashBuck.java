4c64d2919637545d1b3ee5004591633715f3d821
@CX330_x <2810288656@qq.com>
2022-10-23 19:34:05 +0800

    日常

--- src/main/java/JavaDS/MyHashBuck.java
+++ src/main/java/JavaDS/MyHashBuck.java
@@ -7,12 +7,12 @@ package JavaDS;
  * @Version 1.0
  */
 @SuppressWarnings({"all"})
-public class MyHashBuck {
-    static class Node{
-        public int key;
-        public int val;
+public class MyHashBuck<K,V> {
+    static class Node<K,V>{
+        public K key;
+        public V val;
         public Node next;
-        public Node(int key, int val){
+        public Node(K key,V val){
             this.key = key;
             this.val = val;
         }
@@ -20,21 +20,62 @@ public class MyHashBuck {
 
     public  Node[] array;
     public  int usedsize;
+    private static final double LOAD_FACTOR = 0.75;
+    private static final int DEFAULT_SIZE = 8;//默认桶的大小
     public MyHashBuck(){
-          array = new Node[8];
+          array = (Node<K,V>[])new Node[DEFAULT_SIZE];
     }
-    public void  put(int key ,int value){
-        int index = key % array.length;
-        Node cur = array[index];
+    public void  put(K key ,V val){
+        int hashKey = key.hashCode();
+        int index = hashKey % array.length;
+        Node<K,V> cur = array[index];
         while (cur != null){
-            if(cur.key == key){
-                cur = cur.next;
+            if(cur.key.equals(key)){
+                cur.val = val;
+                return;
             }
+            cur = cur.next;
         }
-        Node node = new Node(key,value);
+        Node<K,V> node = new Node(key,val);
         node.next = array[index];
         array[index] = node;
         usedsize++;
+        if( loadFactor() >= 0.75) {
+            resize();
+        }
+    }
+    private void resize() {
+        Node<K,V>[] newArray = new Node[2* array.length];
+        for (int i = 0; i < array.length ; i++) {
+            Node<K,V> cur = array[i];
+            while (cur != null) {
+                Node curNext = cur.next;
+                int hashCur = cur.key.hashCode();
+                int newIndex = hashCur % newArray.length;
+                cur.next = newArray[newIndex];
+                newArray[newIndex] = cur;
+                cur = curNext;
+            }
+        }
+        array = newArray;
     }
 
+
+    private double loadFactor() {
+        return usedsize * 1.0 / array.length;
+    }
+
+
+    public V get(V key) {
+        int hash = key.hashCode();
+        int index = hash % array.length;
+        Node<K,V> cur = array[index];
+        while (cur != null) {
+            if(cur.key.equals(key)) {
+                return cur.val;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
 }
